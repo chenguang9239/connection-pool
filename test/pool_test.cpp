c78476283bd6589d5122261f8a663d27959bc453
wangzhiyi <wangzhiyi@yidian-inc.com>
2020-06-30 22:43:01 +0800

    modify test/pool_test.cpp

--- test/pool_test.cpp
+++ test/pool_test.cpp
@@ -1,4 +1,7 @@
+#include <unistd.h>
+
 #include <iostream>
+#include <thread>
 
 #include "MultiConnectionPool.h"
 #include "RRConnectionPool.h"
@@ -6,6 +9,19 @@
 
 int main() {
   ww::MultiConnectionPoolParam multi_conn_pool_param;
+
+  multi_conn_pool_param.zk_config.address =
+      "xxx:2181";
+  multi_conn_pool_param.zk_config.path =
+      "/xxx";
+  multi_conn_pool_param.zk_config.recvTimeout = 500;
+  multi_conn_pool_param.zk_config.connTimeout = 500;
+  multi_conn_pool_param.zk_config.clientLogPath =
+      "/xxx";
+  multi_conn_pool_param.zk_config.clientLogLevel = 2;
+  multi_conn_pool_param.zk_config.logPath =
+      "/xxx";
+  multi_conn_pool_param.zk_config.logLevel = 1;
 
   multi_conn_pool_param.retry = 3;
 
@@ -19,10 +35,22 @@ int main() {
   ww::RRConnectionPool<ww::ThriftClient, ww::ThriftClientFactory> rr_conn_pool(
       multi_conn_pool_param);
 
-  rr_conn_pool.GetNextConn();
+  if (rr_conn_pool.GetNextConn()) {
+    std::cout << std::endl << "get connection ok" << std::endl;
+  }
 
-  std::cout << "Predict result: "
-            << rr_conn_pool.GetNextConn()->client_ptr_->Predict(3) << std::endl;
+  auto func = [&](int i) {
+    std::cout << "Predict result: "
+              << rr_conn_pool.GetNextConn()->client_ptr_->Predict(i)
+              << std::endl;
+  };
+
+  for (int i = 0; i < 200; ++i) {
+    std::thread t(func, i);
+    t.detach();
+  }
+
+  sleep(10);
 
   return 0;
 }
